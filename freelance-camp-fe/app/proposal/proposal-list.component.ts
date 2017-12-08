2195886e8e942f9bd4c53ec018786c0ffde6cded
Jason Dudley <jason.dudley27@gmail.com>
2017-12-08 13:33:46 -0700

    added test data for proposal and imported to the veiw

--- freelance-camp-fe/app/proposal/proposal-list.component.ts
+++ freelance-camp-fe/app/proposal/proposal-list.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Proposal } from './proposal';
+
 
 @Component({
   moduleId: module.id,
   selector: 'proposal-list',
   templateUrl: 'proposal-list.component.html'
 })
-export class ProposalListComponent { }
+export class ProposalListComponent {
+  proposalOne: Proposal = new Proposal(15, 'Abc Company', 'http://jasondudley.com', 'Ruby on Rails', 150, 120, 15, 'jason@sagebin.com')
+  proposalTwo: Proposal = new Proposal(16, 'Umbrella Company', 'http://jasondudley.com', 'Ruby on Rails', 150, 120, 15, 'jason@sagebin.com')
+  proposalThree: Proposal = new Proposal(17, 'E Corp', 'http://jasondudley.com', 'Ruby on Rails', 150, 120, 15, 'jason@sagebin.com')
+
+  proposals: Proposal[] = [
+    this.proposalOne,
+    this.proposalTwo,
+    this.proposalThree
+  ]
+}
