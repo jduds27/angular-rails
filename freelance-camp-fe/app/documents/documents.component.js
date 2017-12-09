500ae5d9819d439ea19ba09f030021c8b408baea
Jason Dudley <jason.dudley27@gmail.com>
2017-12-09 16:14:20 -0700

    added cards for the docs view

--- freelance-camp-fe/app/documents/documents.component.js
+++ freelance-camp-fe/app/documents/documents.component.js
@@ -15,21 +15,21 @@ var DocumentsComponent = /** @class */ (function () {
                 description: "My description",
                 file_url: "http://google.com",
                 updated_at: '05/25/17',
-                image_url: 'http://google.com',
+                image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
             },
             {
                 title: "My Second Doc",
                 description: "My description",
                 file_url: "http://google.com",
                 updated_at: '05/25/17',
-                image_url: 'http://google.com',
+                image_url: 'https://cdn.pixabay.com/photo/2017/03/30/14/35/startup-2188681_960_720.jpg',
             },
             {
                 title: "My Third Doc",
                 description: "My description",
                 file_url: "http://google.com",
                 updated_at: '05/25/17',
-                image_url: 'http://google.com',
+                image_url: 'https://cdn.pixabay.com/photo/2016/03/09/14/40/office-1246484_960_720.jpg',
             }
         ];
     }
